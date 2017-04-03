3c602c1bc683c37a8d80cf3da44c33a5e140519d
Jason Zheng <jzzheng@stanford.edu>
2017-04-03 13:20:31 -0700

    added framework

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,5 @@
-var currentIndex = 0;
 $(document).ready(function(){
-    $('lo').click(function() {
+    $('lo#a').click(function() {
         $('ul.nav').animate({
             height: "125px",
             marginTop: "0px",
@@ -10,4 +9,27 @@ $(document).ready(function(){
                 }, '1000', 'swing');
         });
     });
+
+    $('lo#r').click(function() {
+        $('ul.nav').animate({
+            height: "125px",
+            marginTop: "0px",
+        }, '500', 'swing', function(){
+            $('li').animate({
+                left: "3682.5px",
+                }, '1000', 'swing');
+        });
+    });
+
+    $('lo#c').click(function() {
+        $('ul.nav').animate({
+            height: "125px",
+            marginTop: "0px",
+        }, '500', 'swing', function(){
+            $('li').animate({
+                left: "3682.5px",
+                }, '1000', 'swing');
+        });
+    });
+
 });
