cf9b3dcc6de5995ef1b2833ca35a65b3c34fc439
Jason Zheng <jzzheng@stanford.edu>
2017-04-03 13:01:48 -0700

    added jquery animations instead of css animations

--- static/js/main.js
+++ static/js/main.js
@@ -1,8 +1,13 @@
 var currentIndex = 0;
-
-$('lo').click(function() {
-    $('ul.nav').animate({
-        height: "125px",
-        marginTop: "0px",
-    }, '500', 'swing');
+$(document).ready(function(){
+    $('lo').click(function() {
+        $('ul.nav').animate({
+            height: "125px",
+            marginTop: "0px",
+        }, '500', 'swing', function(){
+            $('li').animate({
+                left: "-=2250px",
+                }, '1000', 'swing');
+        });
+    });
 });
