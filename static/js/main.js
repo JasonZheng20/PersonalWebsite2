2bb3a878cc72f0b8e1a8155bd20d4b9492ebb655
Jason Zheng <jzzheng@stanford.edu>
2017-04-03 21:28:08 -0700

    Landing page and scrolling bar functioning

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,39 @@
 var headerUp= false;
+var currIndex = 0;
 
 $(document).ready(function(){
+    $('div.large#n').click(function() {
+        if (currIndex == 1) {
+            $("lo > div").removeClass('mini-blue').addClass('mini');
+            $('div#r').addClass('mini-blue');
+            currIndex = 2;
+        }
+        else {
+            $("lo > div").removeClass('mini-blue').addClass('mini');
+            $('div#c').addClass('mini-blue');
+            currIndex = 3;
+        }
+        $('li').animate({
+            left: "-=1682.5px"}, '2000', 'swing');
+    });
+
+    $('div.large#p').click(function() {
+        if (currIndex == 2) {
+            $("lo > div").removeClass('mini-blue').addClass('mini');
+            $('div#a').addClass('mini-blue');
+            currIndex = 1;
+        }
+        else {
+            $("lo > div").removeClass('mini-blue').addClass('mini');
+            $('div#r').addClass('mini-blue');
+            currIndex = 2;
+        }
+        $('li').animate({
+            left: "+=1682.5px"}, '2000', 'swing');
+    });
+
     $('div.mini#a').click(function() {
+        currIndex = 1;
         $("lo > div").removeClass('mini-blue').addClass('mini');
         $(this).addClass('mini-blue');
         if (!headerUp) {
@@ -25,6 +57,7 @@ $(document).ready(function(){
     });
 
     $('div.mini#r').click(function() {
+        currIndex = 2;
         $("lo > div").removeClass('mini-blue').addClass('mini');
         $(this).addClass('mini-blue');
         if (!headerUp) {
@@ -48,6 +81,7 @@ $(document).ready(function(){
     });
 
     $('div.mini#c').click(function() {
+        currIndex = 3;
         $("lo > div").removeClass('mini-blue').addClass('mini');
         $(this).addClass('mini-blue');
         if (!headerUp) {
@@ -65,7 +99,7 @@ $(document).ready(function(){
         }
         else {
             $('li').animate({
-                left: "320px",
+                left: "319.5px",
                 }, '2000', 'swing');
         }
 
