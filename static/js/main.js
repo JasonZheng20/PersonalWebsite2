4c38b4248134934d68a255cea7507af66e2a0e68
Jason Zheng <jzzheng@stanford.edu>
2017-04-03 20:31:54 -0700

    working prototype

--- static/js/main.js
+++ static/js/main.js
@@ -1,51 +1,75 @@
+var headerUp= false;
+
 $(document).ready(function(){
-    $('div.mini#a').click(function() { //on first click, decrease the top y position
-        $(this).css("color", "#1ac6ff");
-        $('div.mini#r').css("color", "#000");
-        $('div.mini#c').css("color", "#000");
-        $('ul.nav').animate({
-            height: "125px",
-            marginTop: "0px",
-        }, '500', 'swing', function(){
+    $('div.mini#a').click(function() {
+        $("lo > div").removeClass('mini-blue').addClass('mini');
+        $(this).addClass('mini-blue');
+        if (!headerUp) {
+        $('.header').animate({ marginTop: "0px"}, '75', 'swing', function() {
+            $('ul.nav').animate({
+                height: "125px",
+                marginTop: "0px",
+            }, '50', 'swing', function(){
+                $('li').animate({
+                    left: "3682.5px",
+                    }, '2000', 'swing');
+            });
+        });
+        headerUp=true;
+        }
+        else {
             $('li').animate({
                 left: "3682.5px",
                 }, '2000', 'swing');
-        });
+        }
     });
 
     $('div.mini#r').click(function() {
-        $(this).css("color", "#1ac6ff");
-        $('div.mini#a').css("color", "#000");
-        $('div.mini#c').css("color", "#000");
-        $('ul.nav').animate({
-            height: "125px",
-            marginTop: "0px",
-        }, '500', 'swing', function(){
+        $("lo > div").removeClass('mini-blue').addClass('mini');
+        $(this).addClass('mini-blue');
+        if (!headerUp) {
+        $('.header').animate({ marginTop: "0px"}, '75', 'swing', function() {
+            $('ul.nav').animate({
+                height: "125px",
+                marginTop: "0px",
+            }, '50', 'swing', function(){
+                $('li').animate({
+                    left: "2000px",
+                    }, '2000', 'swing');
+            });
+        });
+        headerUp=true;
+        }
+        else {
             $('li').animate({
                 left: "2000px",
                 }, '2000', 'swing');
-        });
+        }
     });
 
     $('div.mini#c').click(function() {
-        $(this).css("color", "#1ac6ff");
-        $('div.mini#a').css("color", "#000");
-        $('div.mini#r').css("color", "#000"); 
-        $('ul.nav').animate({
-            height: "125px",
-            marginTop: "0px",
-        }, '500', 'swing', function(){
+        $("lo > div").removeClass('mini-blue').addClass('mini');
+        $(this).addClass('mini-blue');
+        if (!headerUp) {
+        $('.header').animate({ marginTop: "0px"}, '75', 'swing', function() {
+            $('ul.nav').animate({
+                height: "125px",
+                marginTop: "0px",
+            }, '50', 'swing', function(){
+                $('li').animate({
+                    left: "320px",
+                    }, '2000', 'swing');
+            });
+        });
+        headerUp=true;
+        }
+        else {
             $('li').animate({
                 left: "320px",
                 }, '2000', 'swing');
-        });
+        }
+
     });
 
 });
 
-
-/* $('.header').animate({
- *  y position: "0px" }, '800', 'swing', function() {next function});
- *
- *
- */
